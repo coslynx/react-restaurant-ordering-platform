d7903980fd016c42e1363dc5d24110474869c4bd
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-08 16:25:05 +0530

    generated file: `server/config/database.config.js`

---
+++ server/config/database.config.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./env.config');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
